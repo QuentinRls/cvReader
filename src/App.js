691167d41014456c1c0004d08923a9009da5b0b2
quentin.riols@epitech.eu <quentin.riols@epitech.eu>
2025-01-20 08:07:34 +0100

    add analytics2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import './App.css';
 import CvReader from '../src/cvReader';
 import { trackPageView } from './analytics';
